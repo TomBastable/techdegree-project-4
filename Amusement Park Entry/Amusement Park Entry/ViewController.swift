92ae55494c068fcd0d787756cfb7d0ce3b6da3c2
Tom Bastable <bastable@gmail.com>
2019-04-25 10:31:13 +0100

    Intial Project Files

---
+++ Amusement Park Entry/Amusement Park Entry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amusement Park Entry
+//
+//  Created by Tom Bastable on 25/04/2019.
+//  Copyright © 2019 Tom Bastable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
